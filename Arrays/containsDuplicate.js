5881ea4d5efaf6e0af76877dc67921345c829701
Lalhming-muana <97506505+lalhming-muana@users.noreply.github.com>
2022-02-09 20:53:41 +0800

    Create containsDuplicate.js

---
+++ Arrays/containsDuplicate.js
@@ -0,0 +1,10 @@
+var containsDuplicate = function(nums) {
+    let data = new Set();
+    for (let x of nums) {
+        if (data.has(x)) {
+            return true;
+        }
+        data.add(x);
+    }
+    return false;
+};
